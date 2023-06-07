c7e3428ab8eb57f65498174291d1873d2c8eae61
Zach Mitchell <zmitchell@fastmail.com>
2023-06-07 14:19:04 -0600

    zmitchell installed stable.nixpkgs-flox.jq
    flox install jq
      stable.nixpkgs-flox.jq: ∅ -> 1.6-bin, 1.6-man

---
+++ 6/pkgs/default/flox.nix
@@ -0,0 +1,24 @@
+{
+  # Packages
+  # "version" is optional, otherwise the latest is used. Try `flox search`
+  # packages.nixpkgs-flox.figlet = {};
+  # packages.nixpkgs-flox.bat = { version = "0.22.1"; };
+
+  # Aliases available when environment is active
+  # shell.aliases.cat = "bat";
+
+  # Script run upon environment activation
+  # Warning: Be careful when using `${}` in shell hook.
+  #          Due to conflicts with Nix language you have to escape it with '' (two single quotes)
+  #          Example: ` ''${ENV_VARIABLE} `
+  # shell.hook = ''
+  #   echo Flox Environment | figlet
+  # '';
+
+  # Environment variables
+  # environmentVariables.LANG = "en_US.UTF-8";
+  packages.nixpkgs-flox.cowsay = {};
+  packages.nixpkgs-flox.jq = {};
+  packages.nixpkgs-flox.tealdeer = {};
+  packages.flox.flox-prerelease = {};
+  }
